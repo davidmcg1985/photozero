41a0e3793d4473851b4f51e8e742ec7de17aa38f
davidmcg1985 <dmcgweb101@gmail.com>
2016-01-21 10:59:21 +0000

    initial commit uploading of base project

---
+++ photozero/settings/production.py
@@ -0,0 +1,19 @@
+import os
+from django.conf import settings
+import dj_database_url
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+# Parse database configuration from $DATABASE_URL
+DATABASES['default'] =  dj_database_url.config()
+
+# Enable Persistent Connections
+DATABASES['default']['CONN_MAX_AGE'] = 500
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
