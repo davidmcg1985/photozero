aba7efb224da936295cc8e0e0cd9de0d046abfa7
davidmcg1985 <dmcgweb101@gmail.com>
2016-01-21 11:18:06 +0000

    amended static_root

--- photozero/settings.py
+++ photozero/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
