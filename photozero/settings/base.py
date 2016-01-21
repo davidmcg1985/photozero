2a95ca651286f9f5d6be2edaf976c4584d552fab
davidmcg1985 <dmcgweb101@gmail.com>
2016-01-21 11:14:12 +0000

    added whitenoise storage to settings.py

--- photozero/settings/base.py
+++ photozero/settings/base.py
@@ -130,6 +130,10 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 # Media URL and Media Root
 MEDIA_ROOT = 'mediafiles'
 MEDIA_URL = '/media/'
