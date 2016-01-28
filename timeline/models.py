a08c3d75580064a27d98fbb2f461358f348d794a
davidmcg1985 <dmcgweb101@gmail.com>
2016-01-28 13:46:20 +0000

    slight modifications to html templates

--- timeline/models.py
+++ timeline/models.py
@@ -3,20 +3,19 @@ from __future__ import unicode_literals
 from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.db import models
+from django.contrib.auth.models import User
 from django.db.models.signals import pre_save
 from django.utils import timezone
 
-from django.contrib.auth.models import User
 from allauth.account.models import EmailAddress
-
 from allauth.socialaccount.models import SocialAccount
-import hashlib
 
 from imagekit.models import ProcessedImageField
 from imagekit.processors import Transpose, ResizeToFit
 
 from django.utils.text import slugify
-# Create your models here.
+
+import hashlib
 
 
 def upload_location(instance, filename):
@@ -32,10 +31,10 @@ class Photo(models.Model):
 	image = ProcessedImageField(upload_to=upload_location, 
 		null=True, 
 		blank=False,
-		processors=[Transpose(), ResizeToFit(960, 500, False)],
+		processors=[Transpose(), ResizeToFit(1500, 1000, False)],
 		#processors=[Transpose(), ResizeToFit(width=960)],
 		format='JPEG',
-		options={'quality': 50},
+		options={'quality': 60},
 		width_field="width_field",
 		height_field="height_field")
 	height_field = models.IntegerField(default=0)
@@ -44,33 +43,17 @@ class Photo(models.Model):
 	updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
 
- 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, related_name='profile')
- 
-    def __unicode__(self):
-        return "{}'s profile".format(self.user.username)
- 
-    class Meta:
-        db_table = 'user_profile'
- 
-    def account_verified(self):
-        if self.user.is_authenticated:
-            result = EmailAddress.objects.filter(email=self.user.email)
-            if len(result):
-                return result[0].verified
-        return False
- 
-User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
+	def __unicode__(self):
+		return self.title
 
- 
-def profile_image_url(self):
-    fb_uid = SocialAccount.objects.filter(user_id=self.user.id, provider='facebook')
- 
-    if len(fb_uid):
-        return "http://graph.facebook.com/{}/picture?width=40&height=40".format(fb_uid[0].uid)
- 
-    return "http://www.gravatar.com/avatar/{}?s=40".format(hashlib.md5(self.user.email).hexdigest())
+	def __str__(self):
+		return self.title
+
+	def get_absolute_url(self):
+		return reverse("timeline:detail", kwargs={"slug": self.slug})
+
+	class Meta:
+		ordering = ["-timestamp", "-updated"]
 
 
 def create_slug(instance, new_slug=None):
@@ -90,17 +73,31 @@ def pre_save_post_receiver(sender, instance, *args, **kwargs):
 		instance.slug = create_slug(instance)
 
 
-	def __unicode__(self):
-		return self.title
-
-	def __str__(self):
-		return self.title
-
-	def get_absolute_url(self):
-		return reverse("timeline:detail", kwargs={"slug": self.slug})
-
-	class Meta:
-		ordering = ["-timestamp", "-updated"]
-
-
 pre_save.connect(pre_save_post_receiver, sender=Photo)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, related_name='profile')
+    about_me = models.TextField(null=True, blank=True)
+
+    def __unicode__(self):
+        return "{}'s profile".format(self.user.username)
+
+    class Meta:
+        db_table = 'user_profile'
+
+    def profile_image_url(self):
+        """
+        Return the URL for the user's Facebook icon if the user is logged in via Facebook,
+        otherwise return the user's Gravatar URL
+        """
+        fb_uid = SocialAccount.objects.filter(user_id=self.user.id, provider='facebook')
+
+        if len(fb_uid):
+            return "http://graph.facebook.com/{}/picture?width=40&height=40".format(fb_uid[0].uid)
+
+        return "http://www.gravatar.com/avatar/{}?s=40".format(
+            hashlib.md5(self.user.email).hexdigest())
+
+
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
